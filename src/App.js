b2abceb467a4cfe5d45e112d3947a1fc93055cff
AbdulRazaque665 <abdulrazaque.xwave@gmail.com>
2024-09-28 21:43:58 +0500

    components create

--- src/App.js
+++ src/App.js
@@ -1,18 +1,17 @@
 import './App.css';
-import Header from './header';
-import Herosection from './herosection';
-import Footer from './footer'
+import Herosection from './components/herosection';
+import Header from './components/header';
+import Footer from './components/footer';
 import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap/dist/js/bootstrap.bundle.min";
 
-
 function App() {
   return (
-   <div>
-    <Header />
-    <Herosection />
-    <Footer />
-   </div>
+    <div>
+    <Header/>
+    <Herosection/>
+    <Footer/>
+    </div>
   );
 }
 

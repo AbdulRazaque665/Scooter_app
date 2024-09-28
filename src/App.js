8ba263e9bc98f7ad2c1c69e95b8b46d5ae87acc2
AbdulRazaque665 <abdulrazaque.xwave@gmail.com>
2024-09-28 21:18:12 +0500

    creating an scooter app

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import './App.css';
+import Header from './header';
+import Herosection from './herosection';
+import Footer from './footer'
+import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap/dist/js/bootstrap.bundle.min";
+
+
+function App() {
+  return (
+   <div>
+    <Header />
+    <Herosection />
+    <Footer />
+   </div>
+  );
+}
+
+export default App;
